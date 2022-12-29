ea44ab3771de0ec214400acdea274c271312abad
kriie <kceekriee@gmail.com>
2022-12-28 22:49:34 -0800

    task 5 correction2

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,9 +1,14 @@
 #include <stdio.h>
 
+/**
+ * main prints a sentence
+ * return: 0 if excited properly, non-zero otherwise
+ */
+
 int main(void)
 {
 
-	printf("with proper grammar, but the outcome is a piece of art\n");
+	printf("with proper grammar, but the outcome is a piece of art,\n");
 
 	return (0);
 }
