26433c23f1537b9189a257dec91a580b8daaa183
kriie <kceekriee@gmail.com>
2022-12-28 22:44:32 -0800

    task 5 correction

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
-int main(void){
+int main(void)
+{
 
-	printf("with proper grammar, but the outcome is piece of art");
+	printf("with proper grammar, but the outcome is a piece of art\n");
 
 	return (0);
 }
