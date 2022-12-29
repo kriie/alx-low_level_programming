c70c4e954b0b51bbec3859530ff3830db9e8ab33
kriie <kceekriee@gmail.com>
2022-12-29 14:04:03 -0800

    task 2

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+#include <ctype.h>
+
+/**
+ * main - computes into lowercase
+ * Return: 0
+ */
+
+int main(void)
+{
+	char alphabet;
+	char lowerCase;
+	lowerCase = toLower(alphabet);
+	putchar(lowerCase);
+	return (0);
+}
