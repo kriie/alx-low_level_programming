83160c4f048c4c13f7a0fa0c3fd9b4cc4b774028
kriie <kceekriee@gmail.com>
2023-01-01 08:13:10 -0800

    task 1 & 2

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * main - prints alphabet in lower case ten times,
+ */
+
+void print_alphabet_x10(void)
+{
+	char letter;
+	int num;
+
+	for (num = 0; num < 10; ++num)
+	{
+		for (letter = 'a'; letter <= 'z'; letter++)
+			_putchar(letter);
+
+		_putchar('\n');
+	}
+
+}
