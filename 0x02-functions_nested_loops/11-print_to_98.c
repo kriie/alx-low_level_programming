3f934a0297076742ae97657737093a6bdcdc0ec8
kriie <kceekriee@gmail.com>
2023-01-02 05:47:08 -0800

    correction and task 11

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,24 @@
+#include "main.h"
+
+/**
+ * print_t0_98 - print natural numbers
+ * @n: number to start counting from
+ */
+
+void print_to_98(int n)
+{
+	if (n >= 98)
+	{
+		while (n > 98)
+			printf("%d, ", n--);
+
+		printf("%d\n", n);
+	}
+	else
+	{
+		while (n < 98)
+			printf("%d, ", n++);
+
+		printf("%d\n", n);
+	}
+}
